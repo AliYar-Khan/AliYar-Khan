0515bf5a62bcc3ecd7d53d05a17b0b206a047b87
Ali Yar Khan <aliyark145@gmail.com>
2024-06-21 00:37:32 +0500

    init portfolio

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import Navbar from './components/NavBar'
+import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
